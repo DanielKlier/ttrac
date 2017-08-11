f06803a6de3d5969d661930d4e16aaf2b638e8f7
Daniel Klier <klier@redhotmagma.de>
2017-08-12 00:18:32 +0200

    Playing around with react router and redux

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,8 +1,21 @@
 import React from 'react';
+import {bindActionCreators} from 'redux';
+import {push} from 'react-router-redux';
+import {connect} from 'react-redux';
 
-export default () => (
+const Home = props => (
     <div>
-        <h1>Homepage</h1>
-        <p>Hello, world!</p>
+        <h1>Home</h1>
+        <p>Welcome home!</p>
+        <button onClick={() => props.changePage()}>Go to about page via redux</button>
     </div>
-)
+);
+
+const mapDispatchToProps = dispatch => bindActionCreators({
+    changePage: () => push('/about-us')
+}, dispatch);
+
+export default connect(
+    null,
+    mapDispatchToProps
+)(Home);
