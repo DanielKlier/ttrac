64e9729269be2c1179c0e1865f7ae605cbcb11cd
Daniel Klier <klier@redhotmagma.de>
2017-08-12 13:45:11 +0200

    Counter example

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -2,20 +2,54 @@ import React from 'react';
 import {bindActionCreators} from 'redux';
 import {push} from 'react-router-redux';
 import {connect} from 'react-redux';
+import {
+    increment,
+    incrementAsync,
+    decrement,
+    decrementAsync
+} from '../../modules/counter';
 
 const Home = props => (
     <div>
         <h1>Home</h1>
-        <p>Welcome home!</p>
-        <button onClick={() => props.changePage()}>Go to about page via redux</button>
+        <p>Count: {props.count}</p>
+
+        <button onClick={props.increment} disabled={props.isIncrementing}>
+            Increment
+        </button>
+
+        <button onClick={props.incrementAsync} disabled={props.isIncrementing}>
+            Increment async
+        </button>
+
+        <button onClick={props.decrement} disabled={props.isDecrementing}>
+            Decrement
+        </button>
+
+        <button onClick={props.decrementAsync} disabled={props.isDecrementing}>
+            Decrement async
+        </button>
+
+        <button onClick={() => props.changePage()}>Go to about page via redux
+        </button>
     </div>
 );
 
+const mapStateToProps = state => ({
+    count         : state.counter.count,
+    isIncrementing: state.counter.isIncrementing,
+    isDecrementing: state.counter.isDecrementing
+});
+
 const mapDispatchToProps = dispatch => bindActionCreators({
-    changePage: () => push('/about-us')
+    increment     : increment,
+    incrementAsync: incrementAsync,
+    decrement     : decrement,
+    decrementAsync: decrementAsync,
+    changePage    : () => push('/about-us')
 }, dispatch);
 
 export default connect(
-    null,
+    mapStateToProps,
     mapDispatchToProps
 )(Home);
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,6 +1,8 @@
 import {routerReducer} from 'react-router-redux';
 import {combineReducers} from 'redux';
+import counter from './counter';
 
 export default combineReducers({
-    router: routerReducer
+    router: routerReducer,
+    counter
 });
