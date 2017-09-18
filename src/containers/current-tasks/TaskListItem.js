d88b16d10f810f5ba11021d32283b5083eabff3c
Daniel Klier <klier@redhotmagma.de>
2017-09-18 23:13:09 +0200

    refactor TaskListItem from function to class component cosmetic animate running task

--- src/containers/current-tasks/TaskListItem.js
+++ src/containers/current-tasks/TaskListItem.js
@@ -1,33 +1,65 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import TaskListHours from './TaskListHours';
 import TaskListIssue from './TaskListIssue';
 import {Button, Glyphicon} from 'react-bootstrap';
 import TaskListTitle from './TaskListTitle';
 
-export default props => (
-    <li className="list-group-item task-list-item">
-        <div className="task-list-item-badges">
-            <TaskListHours elapsedTime={props.elapsedTime}
-                           className="pull-left"/>
-            {props.jiraIssue && <TaskListIssue jiraIssue={props.jiraIssue}/>}
-        </div>
-        <div className="task-list-item-main">
-            <TaskListTitle title={props.title}
-                           onTextChanged={
-                               text => props.onTaskTitleChanged(text)
-                           }/>
-        </div>
-        <div className="task-list-item-buttons">
-            {props.taskIsRunning === false &&
-            <Button onClick={() => props.onStartProgressClicked()}>
-                <Glyphicon glyph="time"/>
-            </Button>
-            }
-            {props.taskIsRunning === true &&
-            <Button onClick={() => props.onStopProgressClicked()}>
-                <Glyphicon glyph="pause"/>
-            </Button>
-            }
-        </div>
-    </li>
-);
+class TaskListItem extends React.PureComponent {
+    // noinspection JSUnusedGlobalSymbols
+    static PropTypes = {
+        elapsedTime: PropTypes.number,
+        jiraIssue: PropTypes.string,
+        title: PropTypes.string,
+        taskIsRunning: PropTypes.bool,
+        onTaskTitleChanged: PropTypes.func.isRequired,
+        onStartProgressClicked: PropTypes.func.isRequired,
+        onStopProgressClicked: PropTypes.func.isRequired
+    };
+
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const props = this.props;
+        const classNames = [
+            'list-group-item',
+            'task-list-item'
+        ];
+        if (props.taskIsRunning) {
+            classNames.push('running');
+        }
+        const className = classNames.join(' ');
+
+        return (
+            <li className={className}>
+                <div className="task-list-item-badges">
+                    <TaskListHours elapsedTime={props.elapsedTime}
+                                   className="pull-left"/>
+                    {props.jiraIssue && <TaskListIssue jiraIssue={props.jiraIssue}/>}
+                </div>
+                <div className="task-list-item-main">
+                    <TaskListTitle title={props.title}
+                                   onTextChanged={
+                                       text => props.onTaskTitleChanged(text)
+                                   }/>
+                </div>
+                <div className="task-list-item-buttons">
+                    {props.taskIsRunning === false &&
+                    <Button onClick={() => props.onStartProgressClicked()}>
+                        <Glyphicon glyph="time"/>
+                    </Button>
+                    }
+                    {props.taskIsRunning === true &&
+                    <Button onClick={() => props.onStopProgressClicked()}>
+                        <Glyphicon glyph="pause"/>
+                    </Button>
+                    }
+                </div>
+            </li>
+        )
+    }
+}
+
+export default TaskListItem;
