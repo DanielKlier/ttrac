6d6a7bca6e345eff4b65c2f4488b7902cf9ba7e4
Daniel Klier <klier@redhotmagma.de>
2017-09-19 08:09:14 +0200

    Stop the running time log before starting another one

--- src/actions/startTaskProgress.js
+++ src/actions/startTaskProgress.js
@@ -1,11 +1,20 @@
 import {START_TASK_PROGRESS} from '../actions';
+import {stopTaskProgress} from './index';
 
 export default function(taskId) {
-    return {
-        type   : START_TASK_PROGRESS,
-        payload: {
-            taskId,
-            startDate: Date.now()
+    return (dispatch, getState) => {
+        const runningTask = getState().app.runningTask;
+        if (runningTask) {
+            dispatch(
+                stopTaskProgress(runningTask.taskId, runningTask.startDate)
+            );
         }
+        dispatch({
+            type   : START_TASK_PROGRESS,
+            payload: {
+                taskId,
+                startDate: Date.now()
+            }
+        });
     };
 }
