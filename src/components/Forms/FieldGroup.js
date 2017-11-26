87ef7303575b7722921cad9a8bf53db93f970e4b
Daniel Klier <klier@redhotmagma.de>
2017-11-26 09:38:46 +0100

    Add validationState property to FieldGroup

--- src/components/Forms/FieldGroup.js
+++ src/components/Forms/FieldGroup.js
@@ -2,9 +2,9 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import {ControlLabel, FormControl, FormGroup, HelpBlock} from 'react-bootstrap';
 
-function FieldGroup({id, label, help, ...props}) {
+function FieldGroup({id, label, help, validationState, ...props}) {
     return (
-        <FormGroup controlId={id}>
+        <FormGroup controlId={id} validationState={validationState}>
             <ControlLabel>{label}</ControlLabel>
             <FormControl {...props} />
             {help && <HelpBlock>{help}</HelpBlock>}
@@ -16,11 +16,13 @@ FieldGroup.propTypes = {
     id: PropTypes.string.required,
     label: PropTypes.string.required,
     help: PropTypes.string,
+    validationState: PropTypes.oneOf(['success', 'warning', 'error', null]),
     ...FormControl.propTypes
 };
 
 FieldGroup.defaultProps = {
-    help: ''
+    help: '',
+    validationState: null
 };
 
 export default FieldGroup;
