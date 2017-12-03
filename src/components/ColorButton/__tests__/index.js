34a69f11fbe53f470d07eaf4b3510b760d51810e
Daniel Klier <klier@redhotmagma.de>
2017-12-03 13:41:08 +0100

    Add test for ColorButton

---
+++ src/components/ColorButton/__tests__/index.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import {mount} from 'enzyme';
+import toJSON from 'enzyme-to-json';
+import ColorButton from '../index';
+import {Button} from 'react-bootstrap';
+
+const color = '#ffaaff';
+
+test('renders a color button normally', () => {
+    const component = mount(<ColorButton color={color}/>);
+
+    expect(toJSON(component)).toMatchSnapshot();
+});
+
+test('it calls the click handler', () => {
+    const handleClick = jest.fn();
+
+    const component = mount(<ColorButton color={color} onClick={handleClick}/>);
+
+    component.find('Button').simulate('click');
+
+    expect(handleClick).toBeCalledWith(color);
+});
+
+test('cannot be clicked when disabled', () => {
+    const handleClick = jest.fn();
+
+    const component = mount(<ColorButton color={color} onClick={handleClick} disabled={true}/>);
+
+    component.find('Button').simulate('click');
+
+    expect(handleClick).toHaveBeenCalledTimes(0);
+});
+
+test('renders its selected state', () => {
+    const component = mount(<ColorButton color={color} isSelected={true}/>);
+
+    expect(toJSON(component)).toMatchSnapshot();
+});
